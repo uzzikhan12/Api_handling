9d425654ca1cdce50f3c09585224a698b14bd4cd
uzzikhan12 <hk33636625@gmail.com>
2024-09-18 02:33:35 +0500

    complete

--- routes/userroute.js
+++ routes/userroute.js
@@ -3,10 +3,10 @@ const Route = express.Router();
 const UserController = require('../controllers/usercontroller');
 const AuthController = require("../controllers/authcontroller");
 
-Route.get("/", UserController.GetAll);
+Route.get("/",AuthController.protected, UserController.GetAll);
 Route.get("/:id", UserController.GetId);
 Route.post("/", UserController.Post);
-Route.put("/:id", UserController.Edit);
-Route.delete("/:id", UserController.Delete);
+Route.put("/:id",UserController.Edit);
+Route.delete("/:id",UserController.Delete);
 
 module.exports = Route;
